8ccaf1f6226bbd44b4d8f09de22d6e2a5fba260f
Xige Michael Chen <mxchen2001@gmail.com>
2022-04-29 14:29:45 -0500

    preprocess data for prediction

--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 import json
 from model import SketchClassifier
+from preprocess import simplify_drawings
 
 app = FastAPI()
 
@@ -20,22 +21,10 @@ app.add_middleware(
 
 guess_model = pickle.load(open("./models/guess_model.pkl", "rb"))
 
-def format_strokes(data):
-    lines = data['lines'] # data from canvas
-    strokes = [] # data for model
-    for stroke in lines:
-        x_coords, y_coords = [], []
-        for point in stroke['points']:
-            x_coords.append(point['x'])
-            y_coords.append(point['y'])
-        strokes.append([x_coords, y_coords])
-    return f'{strokes}'
-
 @app.post("/save")
 async def save_image(request: Request):
     content = await request.json()
-    stroke_str = format_strokes(json.loads(content["strokes"]))    
-    result = guess_model.predict(stroke_str)
+    processed = simplify_drawings('user', json.loads(content["strokes"]))
+    result = guess_model.predict(f'{processed["drawing"]}')
     return {"class": result[0], "probability": f'{result[1]}'}
-    # return {"class": result[0], "probability": result[1]}
 
