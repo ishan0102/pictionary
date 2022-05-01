b89835d5085a0a48f4a97deec9645a8e548b52ea
Xige Michael Chen <mxchen2001@gmail.com>
2022-05-01 01:21:12 -0500

    improved player interface

--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -4,6 +4,8 @@ import CanvasDraw from 'react-canvas-draw';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 
+import categories from './data/categories.json';
+
 const DEBUG = true;
 const ENDPOINT = DEBUG ? 'http://localhost:8000' : '';
 
@@ -12,16 +14,45 @@ export default function Canvas() {
   let canvas = createRef();
   let uuid = uuidv4();
   const [prediction, setPrediction] = useState(null);
+  const [guess, setGuess] = useState(null);
 
   const handleSave = async (data) => {
-    const res = await axios.post(`${ENDPOINT}/save`, {uuid: uuid, strokes: data});
-    setPrediction(`${res.data.class} with probability ${res.data.probability}`);
+    const res = await axios.post(`${ENDPOINT}/save`, { uuid: uuid, strokes: data });
+    setPrediction({class: res.data.class, probability: res.data.probability});
   }
 
   return (
     <div className="flex flex-col items-center justify-center">
+      { guess &&
+        <div class="absolute bottom-0 left-0 bg-orange-100 border-orange-400 text-orange-700 px-8 py-3 m-3 rounded-xl" role="alert">
+          <p class="text-2xl font-normal">Draw {(/[aeiou]/.test(guess[0])) ? 'an' : 'a'} {guess}</p>
+        </div>
+      }
+
+      { prediction &&
+        <div class="absolute bottom-0 right-0 bg-blue-100 border-blue-500 text-blue-700 px-8 py-3 m-3 rounded-xl" role="alert">
+          <p class="text-2xl font-normal">{prediction.probability < 0.7 ? '🤔' : '😎'} I'm {Math.round(prediction.probability * 10000) / 100}% confident that it's a {prediction.class}</p>
+        </div>
+      }
       {/* Top row of buttons */}
       <div className="flex flex-row justify-center">
+        {/* Undo button */}
+        <button
+          className="block text-xl m-2"
+          onClick={() => {
+            // get random category from categories.json
+            while (true) {
+              const category = categories[Math.floor(Math.random() * categories.length)];
+              if (category !== guess) {
+                setGuess(category);
+                return;
+              }
+            }
+          }}
+        >
+          Play
+        </button>
+
         {/* Undo button */}
         <button
           className="block text-xl m-2"
@@ -51,8 +82,8 @@ export default function Canvas() {
         hideGrid
         lazyRadius={0}
         brushRadius={2.5}
-        brushColor={"#000"} 
-        canvasWidth={750} 
+        brushColor={"#000"}
+        canvasWidth={750}
         canvasHeight={500}
       />
 
@@ -63,11 +94,8 @@ export default function Canvas() {
           handleSave(canvas.getSaveData());
         }}
       >
-        Save
+        Submit
       </button>
-      <div>
-        {prediction}
-      </div>
     </div>
   );
 }
